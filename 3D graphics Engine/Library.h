bdc1aeee709139ea3896258bb910a7bbdf050904
Do0vm <125159238+Do0vm@users.noreply.github.com>
2025-05-08 20:54:34 +0200

    [Add] Ray;Triangle;Scene Setup

--- 3D graphics Engine/Library.h
+++ 3D graphics Engine/Library.h
@@ -1,1 +1,116 @@
 #pragma once
+
+#include <glm.hpp>
+
+using namespace glm;
+
+#define EPSILON 0.00001f
+
+class HitInfo {
+	// Output return what i hit
+public:
+	HitInfo() : t(0.f), hit(false), hitLocation(0.f), normal(0.f),emissive(false){} // constructor
+
+	float t;
+	bool hit;
+	vec3 hitLocation;
+	vec3 normal;
+	vec3 colour;
+	bool emissive;
+
+};
+
+class Ray {
+	// Input did i hit something?
+public:
+
+	Ray() : origin(0.f), direction(0.f){}
+
+	vec3 origin;
+	vec3 direction;
+
+};
+
+class Object {
+public:
+	Object() : colour (0.f), emissive(false){} //construcr
+
+	vec3 colour;
+	bool emissive;
+
+	virtual HitInfo hit(Ray ray) const = 0; // Returns info about whether the ray hit the sphere
+
+};
+
+class Triangle : public Object {
+
+public:
+	Triangle() : v1(0.f),v2(0.f),v3(0.f){}
+
+	vec3 v1, v2, v3;
+
+	HitInfo hit(Ray ray) const {
+		HitInfo hi;
+
+
+		vec3 originalNormal = normalize(cross(v2 - v1, v3 - v1)); // getting normal the vector perp to both said vectrs
+
+		vec3 n = originalNormal;
+
+		if (dot(n, ray.direction) > 0.f)
+			n = -n;
+
+		//case 1 hit nothing
+		if (fabs(dot(n, ray.direction)) < EPSILON)
+			return hi;
+		//case 2 hit nothing
+		float t = (dot(n, v1) - dot(ray.origin, n)) / dot(ray.direction, n);
+
+		if (fabs(t) < 0.005f)
+			return hi;
+
+		vec3 p = ray.origin + ray.direction * t;
+
+		vec3 c1 = cross(v2 - v1, p - v1);
+		vec3 c2 = cross(v3 - v2, p - v2);
+		vec3 c3 = cross(v1 - v3, p - v3);
+
+		//case 3 not inside traingle
+		if (dot(c1, originalNormal) < 0 || dot(c2, originalNormal) < 0 || dot(c3, originalNormal) < 0)
+			return hi;
+
+		//We hit somthing
+		hi.hit = true;
+		hi.t = t;
+		hi.hitLocation = p;
+		hi.normal = n;
+		hi.colour = colour;
+		hi.emissive = emissive;
+
+		return hi;
+	}
+
+};
+
+float random() {
+	return ((float)rand() / RAND_MAX);
+}
+
+//get random vector in hemisphere
+vec3 randomVector() {
+
+
+	vec3 d;
+
+	do {
+
+		d = 2.f * vec3(random(), random(), random()) - vec3(1.f);
+	} while (dot(d, d) > 1.f);
+
+	return normalize(d);
+}
+
+vec3 randomDirection(vec3 normal) {
+
+	return normalize(randomVector() + normal);
+}
--- 3D graphics Engine/main.cpp
+++ 3D graphics Engine/main.cpp
@@ -1,13 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include "Library.h"
+#include <vector>
+
 #define WINDOW_WIDTH 400
 #define WINDOW_HEIGHT 400
 
-
+const vec3 eye = vec3(0.f, 0.f, 5.f);
 
 int main() {
 
+	std::vector<Object*> objects;
+
+	Triangle floor1 = Triangle();
+	floor1.v1 = vec3(1.f, -1.f, 1.f);
+	floor1.v2 = vec3(-1.f, -1.f, -1.f);
+	floor1.v3 = vec3(-1.f, -1.f, 1.f);
+	floor1.colour = vec3(1.f);
+	objects.push_back(&floor1);
+
+	Triangle floor2 = Triangle();
+	floor2.v1 = vec3(1.f, -1.f, 1.f);
+	floor2.v2 = vec3(1.f, -1.f, -1.f);
+	floor2.v3 = vec3(-1.f, -1.f, -1.f);
+	floor2.colour = vec3(1.f);
+	objects.push_back(&floor2);
+
+
 	double* image = new double[WINDOW_WIDTH * WINDOW_HEIGHT *3];
 	memset(image, 0.0, sizeof(double) * WINDOW_WIDTH * WINDOW_HEIGHT * 3); //render pixels
+	double* p = image;
+
 
 	sf::RenderWindow window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), "Ray Tracing"); // Window to display
 
@@ -21,7 +43,66 @@ int main() {
 	sf::Sprite sprite;
 	sprite.setTexture(texture);
 
+
+	//Declaring variable to prevent numerous redeclerations inside main whileloop
+	HitInfo hi, finalHi;
+	Ray ray;
+
+	float i = 0.f, j = 0.f, minT = 10000000.f;
+	int r = 0, g = 0, b = 0;
+	vec3 colour = vec3(0.f);
+
+
 	while (window.isOpen()) {
+
+		p = image;
+
+		for (int y = 0; y < WINDOW_HEIGHT; y++) {
+			for (int x = 0; x < WINDOW_WIDTH; x++) {
+				minT= 10000000.f;
+				finalHi.hit = false;
+				hi.hit = false;
+				colour = vec3(0.f);
+
+				i = (2.f * float(x) / float(WINDOW_WIDTH)) - 1.f;
+				j = (2.f * float(WINDOW_HEIGHT - y) / float(WINDOW_HEIGHT) - 1.f);
+
+				ray.origin = vec3(i, j, 1.2f);
+				ray.direction = normalize(ray.origin - eye);
+
+				for (int k = 0; k < objects.size(); k++) {
+
+					hi = objects[k]->hit(ray);
+					if (hi.hit && hi.t < minT) {
+						finalHi = hi;
+						minT = hi.t;
+					}
+				}
+					
+				if (finalHi.hit) {
+
+					colour = finalHi.colour;
+				}
+				*p += colour.r;
+				p++;
+				*p += colour.g;
+				p++;
+				*p += colour.b;
+				p++;
+			}
+		}
+		p = image;
+
+		for (int e = 0; e < WINDOW_HEIGHT; e++) {
+			for (int f = 0; f < WINDOW_WIDTH; f++) {
+				r = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+				g = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+				b = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+
+				sfImage.setPixel(f, e, sf::Color::Color(r, g, b));
+			}
+		}
+
 
 		texture.loadFromImage(sfImage);
 		sprite.setTexture(texture);
