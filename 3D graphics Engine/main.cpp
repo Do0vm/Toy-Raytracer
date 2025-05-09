9bb690568c229f621302a4a3e8bbae7fbe1254ae
Do0vm <125159238+Do0vm@users.noreply.github.com>
2025-05-09 13:46:44 +0200

    [Add] PathTrace (direct Lighting) and sample system

--- 3D graphics Engine/main.cpp
+++ 3D graphics Engine/main.cpp
@@ -5,7 +5,46 @@
 #define WINDOW_WIDTH 400
 #define WINDOW_HEIGHT 400
 
+
+const int numSamples = 1000;
 const vec3 eye = vec3(0.f, 0.f, 5.f);
+const int maxDepth = 10;
+const float brightness = (2.f * 3.1415926f) * (1.f / float(numSamples));
+
+vec3 pathTrace(Ray ray, std::vector<Object*> objects, int depth){ // Light and global illumin
+
+	if (depth > maxDepth) {
+		return vec3(0.f);
+
+
+
+	}
+
+	double minT = 10000000.f;
+	HitInfo hi, finalHi;
+
+	for (int i = 0; i < objects.size(); i++) {
+		hi = objects[i]->hit(ray);
+
+
+		if (hi.hit && hi.t < minT) {
+			minT = hi.t;
+			finalHi = hi;
+
+		}
+	}
+
+	if (!finalHi.hit)
+		return vec3(0.f);
+
+
+	if (finalHi.emissive)
+		return finalHi.colour;
+
+	return vec3(0.f);
+
+
+	}
 
 int main() {
 
@@ -36,6 +75,8 @@ int main() {
 	light1.v2 = vec3(-0.5f, 0.99f, -0.5f);
 	light1.v3 = vec3(-0.5f, 0.99f, 0.5f);
 	light1.colour = vec3(1.f);
+	light1.emissive = true;
+	
 	objects.push_back(&light1);
 
 	Triangle light2 = Triangle();
@@ -43,6 +84,7 @@ int main() {
 	light2.v2 = vec3(0.5f, 0.99f, -0.5f);
 	light2.v3 = vec3(-0.5f, 0.99f, -0.5f);
 	light2.colour = vec3(1.f);
+	light2.emissive = true;
 	objects.push_back(&light2);
 
 
@@ -73,67 +115,97 @@ int main() {
 	HitInfo hi, finalHi;
 	Ray ray;
 
-	float i = 0.f, j = 0.f, minT = 10000000.f;
+	float i = 0.f, j = 0.f, minT = 10000000.f, s = 0;
 	int r = 0, g = 0, b = 0;
 	vec3 colour = vec3(0.f);
 
 
 	while (window.isOpen()) { // This runs every frame 
 
-		p = image;
+		if (s < numSamples) {
 
-		for (int y = 0; y < WINDOW_HEIGHT; y++) { //Go through every pixel on the screen, one by one.
-			for (int x = 0; x < WINDOW_WIDTH; x++) {//looping from the top-left corner to the bottom-right.
 
-				minT= 10000000.f;//resetting things
-				finalHi.hit = false;//resetting things
-				hi.hit = false;//resetting things
-				colour = vec3(0.f); //resetting things
 
-				i = (2.f * float(x) / float(WINDOW_WIDTH)) - 1.f;
-				j = (2.f * float(WINDOW_HEIGHT - y) / float(WINDOW_HEIGHT) - 1.f); //ScreenPositions to Normalized co ordinates
 
-				ray.origin = vec3(i, j, 1.2f);
-				ray.direction = normalize(ray.origin - eye); //position of ray shot
 
-				for (int k = 0; k < objects.size(); k++) { //For each object in the scene, shoot a ray at it and see if it gets hit
+			p = image;
 
-					hi = objects[k]->hit(ray);
-					if (hi.hit && hi.t < minT) {
-						finalHi = hi; //If we hit something, and it’s closer than anything we hit before, remember it.
-						minT = hi.t;
+
+
+
+
+
+			for (int y = 0; y < WINDOW_HEIGHT; y++) { //Go through every pixel on the screen, one by one.
+				for (int x = 0; x < WINDOW_WIDTH; x++) {//looping from the top-left corner to the bottom-right.
+
+					minT = 10000000.f;//resetting things
+					finalHi.hit = false;//resetting things
+					hi.hit = false;//resetting things
+					colour = vec3(0.f); //resetting things
+
+					i = (2.f * float(x) / float(WINDOW_WIDTH)) - 1.f;
+					j = (2.f * float(WINDOW_HEIGHT - y) / float(WINDOW_HEIGHT) - 1.f); //ScreenPositions to Normalized co ordinates
+
+					ray.origin = vec3(i, j, 1.2f);
+					ray.direction = normalize(ray.origin - eye); //position of ray shot
+
+					for (int k = 0; k < objects.size(); k++) { //For each object in the scene, shoot a ray at it and see if it gets hit
+
+						hi = objects[k]->hit(ray);
+						if (hi.hit && hi.t < minT) {
+							finalHi = hi; //If we hit something, and it’s closer than anything we hit before, remember it.
+							minT = hi.t;
+						}
 					}
+
+					if (finalHi.hit) {
+						if (finalHi.emissive)
+							colour = finalHi.colour; //If we didn’t hit anything, the color stays black
+
+
+						else {
+							Ray randomRay;
+							randomRay.origin = finalHi.hitLocation;
+							randomRay.direction = randomDirection(finalHi.normal);
+
+
+
+							vec3 originalColour = finalHi.colour;
+							vec3 newColour = pathTrace(randomRay, objects, 0);
+							colour = newColour * originalColour;
+
+							colour *= brightness;
+						}
+					}
+					*p += colour.r;
+					p++;
+					*p += colour.g;
+					p++;
+					*p += colour.b;
+					p++;
 				}
-					
-				if (finalHi.hit) {
+			}
+			p = image;
 
-					colour = finalHi.colour; //If we didn’t hit anything, the color stays black
+			for (int e = 0; e < WINDOW_HEIGHT; e++) {
+				for (int f = 0; f < WINDOW_WIDTH; f++) {
+					r = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+					g = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+					b = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+
+					sfImage.setPixel(f, e, sf::Color::Color(r, g, b));
 				}
-				*p += colour.r;
-				p++;
-				*p += colour.g;
-				p++;
-				*p += colour.b;
-				p++;
 			}
+
+
+			texture.loadFromImage(sfImage);
+			sprite.setTexture(texture);
+			s++;
+
+
 		}
-		p = image;
-
-		for (int e = 0; e < WINDOW_HEIGHT; e++) {
-			for (int f = 0; f < WINDOW_WIDTH; f++) {
-				r = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
-				g = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
-				b = clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
-
-				sfImage.setPixel(f, e, sf::Color::Color(r, g, b));
-			}
-		}
-
-
-		texture.loadFromImage(sfImage);
-		sprite.setTexture(texture);
+	
 		sf::Event sfEvent;
-
 		while (window.pollEvent(sfEvent)) {
 
 
